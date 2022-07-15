10de5a0651af6164f22e292531ed0c9ece6e60ab
AVULA KIRAN KUMAR REDDY <103331427+Kiranreddy-2502@users.noreply.github.com>
2022-07-16 01:08:14 +0530

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+/*--------------typing animation-------------*/
+const express = require("express")
+const app = express()
+app.use(express.static(__dirname + '/public'));
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Successfully started 3000");
+})
